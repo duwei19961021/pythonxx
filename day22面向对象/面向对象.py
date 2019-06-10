091aab619e976afdeeeec4f9a1cbdebf8a89a802
duwei <duwei19961021@163.com>
2019-06-10 23:48:52 +0800

    "<add day22 面向对象>"

---
+++ day22面向对象/面向对象.py
@@ -0,0 +1,12 @@
+class first:
+    def email(self,a):
+        print(a)
+    def message(self,b):
+        print(b)
+    def send(self,c):
+        print(c)
+obj = first()
+print(obj)
+obj.email("a")
+obj.message("b")
+obj.send("c")
