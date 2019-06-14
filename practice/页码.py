373ade006e02ad6547c9630e6825583b52191d9e
duwei work <duwei19961021@163.com>
2019-06-14 15:38:43 +0800

    <页码选择器>

---
+++ practice/页码.py
@@ -0,0 +1,13 @@
+class page:
+    def __init__(self,page,perpage):
+        self.page = page
+        self.perpage = perpage
+    @property
+    def startpage(self):
+        return self.perpage*(self.page-1)+1
+    @property
+    def endpage(self):
+        return self.perpage*self.page+1
+obj = page(10,8)
+for pageone in range(obj.startpage,obj.endpage):
+    print(pageone)
