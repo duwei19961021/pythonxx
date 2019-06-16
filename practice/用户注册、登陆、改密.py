b0e2f83857a127e986e0d8edd00732ec4eb45b9e
duwei <duwei19961021@163.com>
2019-06-16 09:56:14 +0800

    "<优化>"

--- practice/用户注册、登陆、改密.py
+++ practice/用户注册、登陆、改密.py
@@ -37,7 +37,7 @@ class user: #用户类
         self.mobile = mobile
 
     @staticmethod
-    def md5(password):  # 密码非明文,静态方法，无需使用对象中封装的值
+    def md5(password):  # password加密,静态方法，无需使用对象中封装的值
         m = hashlib.md5()
         m.update(password.encode("utf8"))
         a_md5 = m.hexdigest()
@@ -55,7 +55,7 @@ class user: #用户类
 
     def checkuser(self):  # 检查用户是否存在
         sql2 = "select user_name from shop.users where user_name='{}';".format(self.username)
-        if self.execsql(sql2) == ():
+        if user.execsql(sql2) == ():
             return 0
         else:
             return 1
@@ -64,17 +64,18 @@ class user: #用户类
         if self.checkuser() == 1:
             print("user had exist!")
         else:
-            sql1 = "insert into shop.users (user_name,Password,mobile) values ('{}','{}',{});".format(self.username, self.md5(self.password),self.mobile)
-            self.execsql(sql1)
+            sql1 = "insert into shop.users (user_name,Password,mobile) values ('{}','{}',{});".format(self.username, user.md5(self.password),self.mobile)
+            user.execsql(sql1)
             print("register successfuly")
+
 
     def modifypwd(self):  # 修改密码
         if self.checkuser() == 1:
             sql = "select mobile from shop.users where user_name='{}';".format(self.username)
-            if eval(self.execsql(sql)[0][0]) == self.mobile:
+            if eval(user.execsql(sql)[0][0]) == self.mobile:
                 newpassword = input("mobile check successfuly and input your newpassword that you want:")
                 sql2 = "update shop.users set password='{}' where user_name = '{}'".format(self.md5(newpassword), self.username)
-                self.execsql(sql2)
+                user.execsql(sql2)
                 print("modify password successfuly!")
             else:
                 print("mobile checking faied")
@@ -83,18 +84,18 @@ class user: #用户类
 
     def login(self):
         sql = "select password from shop.users where user_name='{}'".format(self.username)
-        if self.checkuser() == 1 and self.execsql(sql)[0][0] == self.md5(self.password):
+        if self.checkuser() == 1 and user.execsql(sql)[0][0] == self.md5(self.password):
             print("login successfuly")
         else:
             print("your username or password error!")
-
-obj = user("duwei","1234566",13487459075)
-choice = eval(input("input your choice:"))
+print("1、注册 2、登录 3、修改密码")
+choice = int(input("input your chice:"))
 if choice == 1:
-    obj.registered()
-elif choice ==2:
-    obj.login()
+    obj1 = user("duwei3",'123456',13487459076)
+    obj1.registered()
+elif choice == 2:
+    obj2 = user("duwei",'duwei',13487459076)
+    obj2.login()
 elif choice == 3:
-    obj.modifypwd()
-
-
+    obj3 = user("duwei","duwei",13487459076)
+    obj3.modifypwd()
