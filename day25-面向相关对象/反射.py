25ea670856a0ce4bc96823ccd6fedea1215599fe
duwei <duwei19961021@163.com>
2019-06-16 23:12:03 +0800

    "<反射>"

---
+++ day25-面向相关对象/反射.py
@@ -0,0 +1,7 @@
+ # -*- coding: utf-8 -*-
+# @Time    : 2019/6/16 23:04
+# @Author  : duwei
+# @FileName: 反射.py
+# @Software: PyCharm
+# @Blog    ：https://github.com/duwei19961021/pythonxx
+getattr()
