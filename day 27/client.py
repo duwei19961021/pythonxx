5adf3eb0795af78d065c5a3073e5097a67d4f177
duwei <duwei19961021@163.com>
2019-06-28 00:21:19 +0800

    "<day27>"

--- day 27/client.py
+++ day 27/client.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
+# import socket
+# sk = socket.socket()
+# sk.connect(('192.168.5.175',80))
+# # sk.connect(('192.168.5.175',80))
+# while True:
+#     name = input(">>>:")
+#     sk.send(name.encode('utf-8'))
+#     if name == 'exit':
+#         break
+#     response = sk.recv(1024)
+#     print(response.decode('utf-8'))
+# sk.close()
 import socket
-sk = socket.socket()
-sk.connect(('192.168.5.175',80))
-while True:
-    name = input(">>>:")
-    sk.send(name.encode('utf-8'))
-    if name == 'exit':
-        break
-    response = sk.recv(1024)
-    print(response.decode('utf-8'))
-sk.close()
+client = socket.socket()
+client.connect(('192.168.5.175',80))
+client.send(b'duwei')
+print(client.recv(1024))
+client.close()
--- day 27/server.py
+++ day 27/server.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
-import socket
-server = socket.socket()
-server.bind(('192.168.5.175',80))
-server.listen(5)
-while True:
+# import socket
+# server = socket.socket()
+# server.bind(('192.168.5.175',80))
+# server.listen(5)
+# while True:
+#     conn,addr = server.accept()
+#     while True:
+#         data = conn.recv(1024)
+#         if data == b'exit':
+#             break
+#         response = data + b'sb'
+#         conn.send(response)
+#         print(conn.recv(1024))
+#     conn.close()
+while 1:
+    import socket
+    server = socket.socket()
+    server.bind(('192.168.5.175',80))
+    server.listen(5)
     conn,addr = server.accept()
-    while True:
-        data = conn.recv(1024)
-        if data == b'exit':
-            break
-        response = data + b'sb'
-        conn.send(response)
+    data = conn.recv(1024)
+    response = data + b'666'
+    conn.send(response)
+    print(data)
+    conn.send(data + b"666")
     conn.close()
+    server.close()
